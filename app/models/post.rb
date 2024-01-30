75aba05713273850fd456376e0385ca492283c3f
Manjilbhuju <bhujumanjil727@gmail.com>
2024-01-30 16:32:14 +0545

    image attached

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: {minimum: 5, maximum: 100}
     validates :description, presence: true, length:{minimum: 10, maximum: 1000}
     validates :keywords, presence: true, length:{minimum:4, maximum: 20}
+    has_one_attached :image
 end
